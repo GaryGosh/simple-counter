58b718a87c92714ee09e4d38ca56d011aaa8d11b
GaryGosh <clichethe1@yahoo.com>
2020-07-04 18:05:55 +0530

    Updates

---
+++ precache-manifest.aa76cb631d7c432e6016dd1721f7d383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7aa0a05af3146801d6b97a1df757338",
+    "url": "/Simple-Counter/index.html"
+  },
+  {
+    "revision": "47aa0da741dea54ba1a1",
+    "url": "/Simple-Counter/static/css/main.9d0bac78.chunk.css"
+  },
+  {
+    "revision": "297ec8e3f45cacae2835",
+    "url": "/Simple-Counter/static/js/2.63252aec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-Counter/static/js/2.63252aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47aa0da741dea54ba1a1",
+    "url": "/Simple-Counter/static/js/main.54645179.chunk.js"
+  },
+  {
+    "revision": "d87795e8372db5d3617f",
+    "url": "/Simple-Counter/static/js/runtime-main.950f8fc2.js"
+  }
+]);
